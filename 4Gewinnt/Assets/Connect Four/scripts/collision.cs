0a5342edb7282e6bc2c909649fabb5646e55a8d5
Hinnaaak <maltehansen95@gmx.de>
2018-12-08 20:10:01 +0100

    always something is missing

---
+++ 4Gewinnt/Assets/Connect Four/scripts/collision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour {
+
+    public bool blockColl = false;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!blockColl)
+        {
+            blockColl = true;
+            GameController parentScript = transform.parent.GetComponent<GameController>();
+            parentScript.CollisionFromChild(transform.tag, other.gameObject.name);
+        }
+    }
+
+    public void unblockCollision()
+    {
+        blockColl = false;
+    }
+}
